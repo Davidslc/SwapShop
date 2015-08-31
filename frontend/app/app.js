6fc2665273de146b9f303638a1199ba9126fd644
Davidslc <djjohnson@gmail.com>
2015-08-31 15:41:50 -0600

    removed authentication to get swaps working again. Going to start auth from scratch

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -10,43 +10,17 @@ angular.module('myApp', [
     'myApp.myItems',
     'myApp.itemDetail',
     'myApp.addItem',
-    'myApp.version',
-    'myApp.login',
-    'myApp.register'
+    'myApp.version'
 ])
     .config(['$routeProvider', "RestangularProvider", function ($routeProvider, RestangularProvider) {
         $routeProvider.otherwise({redirectTo: '/swap-gallery'});
 
         RestangularProvider.setBaseUrl('http://localhost:8001');
         //RestangularProvider.setDefaultHeaders({'Content-Type': undefined});
-    }])
-
-    .run(['$cookieStore', '$rootScope', '$http', '$location', 'Restangular', function ($cookieStore, $rootScope, $http, $location, Restangular) {
-        if ($cookieStore.get('djangotoken')) {
-            $http.defaults.headers.common['Authorization'] = 'Token ' + $cookieStore.get('djangotoken');
-            //document.getElementById("main").style.display = "block";
-            //$location.path('/login')
-        } else {
-            $location.path('/login')
-        }
-
-        $http.get('http://localhost:8001/');
-
-        // Add auth token to every Restangular request
-        Restangular.setFullRequestInterceptor(function(element, operation, route, url, headers, params) {
-
-            var token = $cookieStore.get('djangotoken');
-            if (token) {
-                headers['Authorization'] = 'Token ' + token;
-            }
-
-            headers['X-CSRFToken'] = $cookieStore.get('csrftoken');
-
-            return { element: element, params: params, headers: headers }
-        });
-
     }]);
 
+
+// What is this?
 angular.module('SampleFormDefinition', [])
     .value(
     'SampleForm', {
